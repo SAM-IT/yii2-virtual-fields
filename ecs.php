8ec891d71dc87913a6e6538e5eef8f9668afa4e9
Sam Mousa <sam@mousa.nl>
2022-09-13 15:12:18 +0200

    ci: dont test generated file code style

--- ecs.php
+++ ecs.php
@@ -16,6 +16,10 @@ return static function (ECSConfig $ecsConfig): void {
         __DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php'
     ]);
 
+    $ecsConfig->skip([
+        __DIR__ . '/tests/_support/_generated'
+    ]);
+
     $ecsConfig->import(SetList::PSR_12);
 
     $ecsConfig->ruleWithConfiguration(ArraySyntaxFixer::class, [
